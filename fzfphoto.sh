ccfaca375b4baefca27a4d82466f5bbcebb62e5f
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-02-15 19:14:47 -0500

    Update fzfphoto.sh

--- fzfphoto.sh
+++ fzfphoto.sh
@@ -1,3 +1,3 @@
 # Use fzf to open an image
 # It automatically installs dependencies and updates them
-sudo apt install fzf feh && feh $(fzf --preview='echo "Open {} as an image"')
+sudo apt install fzf chafa && chafa $(fzf --preview='echo "Open {} as an image"')
