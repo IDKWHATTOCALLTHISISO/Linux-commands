aff41890e49607699cecfb03fc75867df052c2a4
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-02-15 20:52:34 -0500

    Update and rename fzfphoto.sh to fuzzyphoto.sh

---
+++ fuzzyphoto.sh
@@ -0,0 +1,1 @@
+sudo apt install fzf chafa feh && feh $(fzf --preview='chafa -s $(tput cols)x$(tput lines) {} && echo "The image may not look like this."')
--- fzfphoto.sh
+++
@@ -1,3 +0,0 @@
-# Use fzf to open an image
-# It automatically installs dependencies and updates them
-sudo apt install fzf chafa && chafa $(fzf --preview='echo "Open {} as an image"')
