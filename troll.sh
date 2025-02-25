a3c650c49d8d1a098c3a95b3c3bd659bbd3909c1
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-02-25 17:47:54 -0500

    Update troll.sh

--- troll.sh
+++ troll.sh
@@ -1,2 +1,2 @@
 # Youve been trolled youve been trolled yes youve porbally been told
-echo lol
+curl https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script
